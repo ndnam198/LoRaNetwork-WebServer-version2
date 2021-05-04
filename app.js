9329453086ee9801808f8b82b7e3840839d753ff
ndnam198 <ndnam198@gmail.com>
2021-05-05 00:04:16 +0700

    merge 2 port

--- app.js
+++ app.js
@@ -1,42 +1,167 @@
+require('console-stamp')(console, {
+    format: ':date().blue (->).blue'
+});
 var express = require('express');
 const { readSync } = require('fs');
 var app = express();
 app.use("/public", express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const io = require('socket.io')(server);
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var convert = require('xml-js');
 var modifier = require('./scripts/xmlModifier');
+var constant = require('./scripts/constants');
+var Server = require('simple-websocket/server');
+const { default: consoleStamp } = require('console-stamp');
 
-const PORT = process.env.PORT || 3001;
-
-server.listen(PORT, function () {
-    console.log("listening...");
+// var server2 = new Server({ port: process.env.WEB_PORT || 5000 }) // see `ws` docs for other options
+const WEB_PORT = process.env.PORT || 3000;
+const PI_PORT = process.env.PORT2 || 3001;
+var server2 = new Server({ server: server, path: "/pi" })
+server.listen(WEB_PORT, function () {
+    console.log("web_socket listening on port " + WEB_PORT + "...");
 });
 
-io.on('connection', function (socket) {
-    console.log('new connection ' + socket.id);
-    socket.emit('updateData', modifier.updateControlPage());
+// see `ws` docs for other options
 
-    socket.on('controlAll', function (data) {
-        console.log(data);
-        var ret = modifier.modifyAll(data.status, function () {
-            io.sockets.emit('updateData', modifier.updateControlPage());
+var isLogin = false;
+var id = 0;
+var pi_id = 0
+var web_sock_list = {};
+var pi_sock_list = {}
+
+try {
+    console.log('pi_socket listening on port: ' + server2.opts.port + ' ' + server2['_server']['options']['port'] + ' ' + PI_PORT);
+    server2.on('connection', function (pi_socket) {
+        pi_socket.id = pi_id++;
+        pi_socket.userList = {};
+        pi_sock_list[pi_socket.id] = pi_socket
+        console.log("pi connected, id: " + pi_socket.id)
+        io.sockets.emit('reload', {});
+
+        pi_socket.on('data', function (bData) {
+            data = bData.toString();
+            if (data.includes('ping')) {
+                i = data.replace(/[^0-9]/g, '');
+                pi_socket.write(`pong ${i}`)
+            }
+            else {
+                console.log("[" + pi_socket.id + "] pi's data: ", data);
+                let msg = data.split('.')
+                console.log(JSON.stringify(msg))
+                modifier.modifyNode(
+                    msg[constant.MSG_INDEX['HEADER_SOURCE_ID']],
+                    msg[constant.MSG_INDEX['DATA_LOCATION']],
+                    msg[constant.MSG_INDEX['DATA_RELAY_STATE']],
+                    msg[constant.MSG_INDEX['DATA_ERR_CODE']],
+                    function () {
+                        for (const i in pi_socket.userList) {
+                            // console.log('----> emit updateData to userID: ', i);
+                            pi_socket.userList[i].emit('updateData', modifier.getDbAsObj());
+                        }
+                    }
+                );
+            }
+        });
+
+        pi_socket.on('close', function () {
+            console.log("pi disconnect, id: " + pi_socket.id);
+            for (const i in pi_socket.userList) {
+                console.log('----> emit gatewayDisconnect to userID: ', i);
+                pi_socket.userList[i].emit('gatewayDisconnect', {});
+            }
+            console.log('>>>>> delete pi_socket.id: ', pi_socket.id);
+            delete pi_sock_list[pi_socket.id];
+
+        });
+
+        pi_socket.on('error', function (err) {
+            console.log(err)
+        });
+
+        io.on('connection', function (web_socket) {
+            web_socket.id = id++;
+            web_socket.login = isLogin;
+            web_sock_list[web_socket.id] = web_socket;
+            pi_socket.userList[web_socket.id] = web_socket;
+
+            console.log('new user connection, id: ' + web_socket.id);
+            web_socket.emit('updateData', modifier.getDbAsObj());
+            web_socket.emit('sessionID', web_socket.id);
+            console.log('----> emit gatewayConnect');
+            web_socket.emit('gatewayConnect', {})
+
+
+            web_socket.on('controlAll', function (data) {
+                if (web_socket.login == true) {
+                    dbAsJson = modifier.getDbAsJson()
+                    console.log(data)
+                    let msg = {}
+                    for (let i = 0; i < dbAsJson.data.nodedata.node.length; i++) {
+                        msg['nodeID'] = dbAsJson.data.nodedata.node[i].nodeID._text;
+                        msg['status'] = data['status'];
+                        msg['opcode'] = constant.OPCODE['REQUEST_RELAY_CONTROL']
+                        pi_socket.write(JSON.stringify(msg));
+                        console.log(msg)
+                    }
+                }
+            });
+
+            web_socket.on('controlSingle', function (data) {
+                if (web_socket.login == true) {
+                    data['opcode'] = constant.OPCODE['REQUEST_RELAY_CONTROL']
+                    console.log(data);
+                    pi_socket.write(JSON.stringify(data))
+                }
+            });
+
+            web_socket.on('disconnect', function () {
+                console.log('user disconnect, id: ' + web_socket.id);
+                web_socket.login = false;
+                console.log('>>>>> delete web_socket.id: ', web_socket.id);
+                delete pi_socket.userList[web_socket.id]
+                delete web_sock_list[web_socket.id];
+            });
         });
     });
+} catch (error) {
+    console.log('----- ERROR')
+    console.log(err)
+}
 
-    socket.on('controlSingle', function (data) {
-        console.log(data);
-        var ret = modifier.modifyNode(data.nodeID, null, data.status, null, function () {
-            io.sockets.emit('updateData', modifier.updateControlPage());
-        });
-    });
-
-    socket.on('disconnect', function () {
-        console.log('user disconnect ' + socket.id);
-    });
-
-});
 
 app.get('/', function (req, res) {
-    res.render('control');
+    console.log(req.url);
+    isLogin = false
+    res.render('index');
+});
+
+app.get('/control', function (req, res) {
+    console.log(req.url);
+    if (isLogin == true)
+        res.render('control');
+    else
+        res.sendStatus(404);
+});
+
+app.post("/login", urlencodedParser, function (req, res) {
+    console.log(req.url);
+    if (!req.body) return res.sendStatus(400);
+    var ret = modifier.loginParser(req.body.username, req.body.password);
+    if (ret == 1) {
+        /* login ok */
+        console.log('logged in with username: ');
+        console.log(req.body.username);
+
+        isLogin = true;
+        res.status(200).json({ status: "ok" });
+    }
+    else {
+        console.log("Invalid login");
+        res.sendStatus(404);
+    }
+    res.end();
 });
--- scripts/xmlModifier.js
+++ scripts/xmlModifier.js
@@ -15,15 +15,16 @@ var options = {
 var xmlPath = __dirname + '/../data/nodeData.xml';
 
 function modifyXml(jsonContent, callback) {
-    fs.writeFile(xmlPath, formatXml(xmlParser.js2xml(jsonContent, options), { collapseContent: true }), function (err, currentXml) {
+    fs.writeFileSync(xmlPath, formatXml(xmlParser.js2xml(jsonContent, options), { collapseContent: true }), function (err, currentXml) {
         if (err) {
             console.log("err")
         } else {
-            console.log("Xml file successfully updated.")
+            console.log("Xml file was successfully updated.")
         }
     })
-    setTimeout(callback, 40);
-    return;
+    if (callback !== null)
+        callback()
+    // setTimeout(callback, 100);
 }
 
 module.exports = {
@@ -35,9 +36,10 @@ module.exports = {
         for (let i = 0; i < 3; i++) {
             if (currentXml.data.nodedata.node[i].nodeID._text == id) {
                 /* if node has no error */
-                if (currentXml.data.nodedata.node[i].error._text == null)
-                    if (currentXml.data.nodedata.node[i].status._text != status && status != null)
-                        currentXml.data.nodedata.node[i].status._text = status;
+                console.log('update node id = ' + id);
+                console.log('update node id = ' + id);
+                if (currentXml.data.nodedata.node[i].status._text != status && status != null)
+                    currentXml.data.nodedata.node[i].status._text = status;
                 if (currentXml.data.nodedata.node[i].location._text != location && location != null)
                     currentXml.data.nodedata.node[i].location._text = location;
                 if (currentXml.data.nodedata.node[i].error._text != err && err != null)
@@ -45,7 +47,8 @@ module.exports = {
             }
         }
         modifyXml(currentXml, function () {
-            callback();
+            if (callback !== null)
+                callback();
         });
         return 1;
     },
@@ -56,17 +59,16 @@ module.exports = {
         if (!currentXml || !currentXml.data.nodedata || !Array.isArray(currentXml.data.nodedata.node))
             return;
         for (let i = 0; i < 3; i++) {
-            /* if node has no error */
-            if (currentXml.data.nodedata.node[i].error._text == null)
-                currentXml.data.nodedata.node[i].status._text = status;
+            currentXml.data.nodedata.node[i].status._text = status;
         }
         modifyXml(currentXml, function () {
-            callback();
+            if (callback !== null)
+                callback();
         });
         return 1;
     },
 
-    updateControlPage: function () {
+    getDbAsObj: function () {
         var xml = fs.readFileSync(xmlPath, 'utf8');
         var currentXml = xmlParser.xml2js(xml, options);
         if (!currentXml || !currentXml.data.nodedata || !Array.isArray(currentXml.data.nodedata.node))
@@ -78,7 +80,28 @@ module.exports = {
             nodeData[`node${i}_status`] = currentXml.data.nodedata.node[i].status._text.toUpperCase();
             nodeData[`node${i}_error`] = currentXml.data.nodedata.node[i].error._text;
         }
-        console.log('update data');
+        console.log('update /control');
         return (nodeData);
-    }
+    },
+
+    getDbAsJson: function () {
+        return xmlParser.xml2js(fs.readFileSync(xmlPath, 'utf8'), options);
+    },
+
+    loginParser: function (username, password) {
+        var xml = fs.readFileSync(xmlPath, 'utf8');
+        var currentXml = xmlParser.xml2js(xml, options);
+        if (!currentXml || !currentXml.data.users || !currentXml.data.users.user || !Array.isArray(currentXml.data.users.user))
+            return;
+        /* Return control page if username and password exist in database */
+        for (let i = 0; i < currentXml.data.users.user.length; i++) {
+            if (username == currentXml.data.users.user[i].username._text) {
+                if (password == currentXml.data.users.user[i].password._text) {
+                    return 1;
+                }
+            }
+        }
+        return;
+    },
+
 }
